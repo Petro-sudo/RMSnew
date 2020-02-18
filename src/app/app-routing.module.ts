3bcae42960418bd7ef055ebc6dbbbc663307ec6a
Petro-sudo <petro.malele@gmail.com>
2020-02-18 17:28:20 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,10 @@ const routes: Routes = [
   },  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'forget',
+    loadChildren: () => import('./forget/forget.module').then( m => m.ForgetPageModule)
   }
 
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
     },
     {
       title: 'Login',
-      url: '/folder/Login',
+      url: 'login',
       icon: 'log-in'
     },
     {
