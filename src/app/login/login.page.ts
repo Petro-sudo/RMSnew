0123649cfab855bd50dbd43abcc0869622148aae
Petro-sudo <petro.malele@gmail.com>
2020-02-20 13:10:50 +0200

    login connect

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import {ServiceService} from './../service.service';
 import {ReactiveFormsModule, FormGroup, FormControl, Validators} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthConstants } from '../config/auth-constant';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +12,54 @@ import {HttpClient} from '@angular/common/http';
 })
 export class LoginPage implements OnInit {
 
-  @Input() loginData= {email:" ", password:" "}
-  navCtrl: any;
-  _ServiceService: any;
+  //@Input() loginData= {email:" ", password:" "}
+  //navCtrl: any;
+  //_ServiceService: any;
+public postData = {email:" ", password:" "
 
-  constructor(private _serviceService : ServiceService) { }
+};
+  constructor(private router: Router,
+     private _serviceService : ServiceService) { }
 login: any=[];
 
   ngOnInit() {
-    this.getData();
+    //this.getData();
   }
+  validateInputs(){
+    let email = this.postData.email.trim();
+    let password = this.postData.password.trim();
+    return (this.postData.email && 
+      this.postData.password && 
+      email.length>0 
+      && password.length>0)
+  
+  }
+
+  loginAction(){
+    if(this.validateInputs()){
+this._serviceService.login(this.postData).subscribe((res:any)=>{
+  if(res.postData.email && res.postData.password){
+this._serviceService.store(AuthConstants,AUTH, res.postData.email && res.postData.email);
+this.router.navigate(['home']);
+  }else
+  {
+    console.log("invalid info")
+  }
+})
+    }else{
+      console.log("please give info");
+    }
+  }
+
+
+
+
+
+
+
+
+
+
  public getData()
 {
     this._serviceService.getLogin().subscribe((data: any)=>
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  store(AuthConstants: typeof import("./config/auth-constant").AuthConstants, AUTH: any, arg2: any) {
+    throw new Error("Method not implemented.");
+  }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _serviceService: ServiceService) { }
   login_url = 'http://168.172.185.101:7000/st';
   reg_url = 'http://168.172.185.106:7000/addlord';
   regstu_url = 'http://168.172.185.106:5000/reg';
@@ -16,14 +20,20 @@ export class ServiceService {
   {
     return this.http.get<any>(this.login_url);
   }
- 
+
+
+  login(postData:any): Observable<any>{
+
+    return this.http.post('login_url',postData)
+  }
+  
+  
+  
   
   public postLandlord(addlord:any)
   {
     return this.http.post<any>(this. reg_url, addlord, {});
   }
-
-
 
   public poststu(reg:any)
   {
